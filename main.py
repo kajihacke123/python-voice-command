aeca43e5cd1084035f36140c050cb4b51466478d
kajihacke123 <kaji.kanlapat99@gmail.com>
2024-12-24 21:40:07 +0700

    update3

--- main.py
+++ main.py
@@ -64,7 +64,58 @@ def dekdoydev(data):
         data = data.split(" ")
         speak("รอสักครู่ครับพี่เกจิ เดี๋ยวเปิดแผนที่ประเทศไทยให้ครับ")
         os.system("start https://www.google.co.th/maps/place/t...")
-    
+
+    if "เปิด Google" in data:
+        speak("เปิด google ให้ครับ")
+        os.system("start https://www.google.co.th/")
+
+    if "เปิดกล้อง" in data:
+        speak("เปิด camera ให้ครับ")
+        os.system("start microsoft.windows.camera:")
+
+    if "เปิด obs" in data:
+        speak("เปิด obs ให้ครับ")
+        current_dir = os.getcwd()  # Save the current working directory
+        obs_path = r"C:\Program Files\obs-studio\bin\64bit"
+        os.chdir(obs_path)  # Change to the OBS directory
+        os.system("obs64.exe")
+        os.chdir(current_dir)  # 
+
+    if "เปิด Messenger" in data:
+        speak("เปิด Messenger ให้ครับ")
+        os.system("start https://www.messenger.com/")
+
+    if "เปิดการตั้งค่าเสียง" in data:
+        speak("เปิดการตั้งค่าเสียง ให้ครับ")
+        os.system("start ms-settings:sound")
+
+    if "ขอเพลงแจ๊ส" in data:
+        speak("เปิดเพลง jazz ให้ครับ")
+        os.system("start https://music.youtube.com/watch?v=eOrGawCeFRQ&list=RDATgy")
+
+    if "ปิดคอม" in data:
+        speak("ปิดคอม ให้ครับ")
+        os.system("shutdown /s /t 1")
+
+    if "เปิด tiktok" in data:
+        speak("เปิด tiktok ให้ครับ")
+        os.system("start https://www.tiktok.com/")
+
+    if "ขอเพลงเพราะๆ" in data:
+        speak("เปิดเพลงเพราะ ให้ครับ")
+        os.system("start https://music.youtube.com/watch?v=JGwWNGJdvx8&list=RDAMVMJGwWNGJdvx8")
+
+    if "เปิด Youtube" in data:
+        speak("เปิด Youtube ให้ครับ")
+        os.system("start https://www.youtube.com/")
+
+    if "เปิดแชท gpt" in data:
+        speak("เปิด chatgpt ให้ครับ")
+        os.system("start https://chat.openai.com/")
+
+    if "เปิด Steam" in data:
+        speak("เปิด steam ให้ครับ")
+        os.system("start steam")
 #Starting Conversation
 
 time.sleep(2)
